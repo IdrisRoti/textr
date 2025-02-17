ea52bb7e6aeffa0bd257549dd899235b5d41549e
IdrisRoti <idrisomisakin@gmail.com>
2025-02-17 19:33:56 +0100

    feat: implement text translation functionality

--- components/TextInterface.tsx
+++ components/TextInterface.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import React, { useEffect } from 'react'
+import React, { FormEvent, useState } from 'react'
 
-const detectLang = async () => {
+const detectLang = async (LangToDetect: string) => {
     // @ts-expect-error type of self.ai is unknown
     const languageDetectorCapabilities = await self.ai.languageDetector.capabilities();
     const canDetect = languageDetectorCapabilities.languageAvailable('en');
@@ -19,36 +19,87 @@ const detectLang = async () => {
     } else {
     // @ts-expect-error type of self.translation is unknown
     detector = await self.ai.languageDetector.create({
-        // @ts-expect-error type of self.translation is unknown
+        // @ts-expect-error type of m is unknown
         monitor(m) {
-            // @ts-expect-error type of self.translation is unknown
+            // @ts-expect-error type of e is unknown
         m.addEventListener('downloadprogress', (e) => {
             console.log(`Downloaded ${e.loaded} of ${e.total} bytes.`);
         });
             },
         });
     }
-    const result= await detector.detect("Hola, Como te llamas?");
+    const result= await detector.detect(LangToDetect);
     const {detectedLanguage} = result[0]
     console.log(detectedLanguage)
     return detectedLanguage;
 }
 
-const translateText = async () => {
+const translateText = async (sourceLang: string, targetLang: string, LangToTranslate: string) => {
     // @ts-expect-error type of self.ai is unknown
     const translatorCapabilities = await self.ai.translator.capabilities();
-    const canTranslate = translatorCapabilities.languagePairAvailable('es', 'fr');
+    const canTranslate = translatorCapabilities.languagePairAvailable(sourceLang, targetLang);
     console.log("canTranslate", canTranslate);
 
+    let translator;
+
+    if (canTranslate === "no"){
+        // The language translator isn't usable.
+        console.log("Sorry your text cannot be translated")
+        return;
+    }
+
+    if (canTranslate === "readily"){
+        console.log("Translating...")
+        // @ts-expect-error type of self.ai is unknown
+        translator = await self.ai.translator.create({
+            sourceLanguage: sourceLang,
+            targetLanguage: targetLang,
+          });
+    } else {
+        console.log("Have to download")
+        // @ts-expect-error type of self.ai is unknown
+        translator = await self.ai.translator.create({
+            sourceLanguage: sourceLang,
+            targetLanguage: targetLang,
+            // @ts-expect-error type of m is unknown
+            monitor(m) {
+                // @ts-expect-error type of e is unknown
+              m.addEventListener('downloadprogress', (e) => {
+                console.log(`Downloaded ${e.loaded} of ${e.total} bytes.`);
+              });
+            },
+          }); 
+          console.log("translator", translator);
+    }
+
+    const result= await translator.translate(LangToTranslate);
+    return result;
 }
 
 const TextInterface = () => {
-    useEffect(() => {
-        translateText();
-    }, [])
+    const [inputText, setInputText] = useState("Thank you so much");
+    const [translatedText, setTranslatedText] = useState("");
+    
+        const handleText = async (e: FormEvent) => {
+            e.preventDefault()
+            const lang:string = await detectLang(inputText);
+            console.log("Language detected",lang)
+
+            const result = await translateText(lang, "fr", inputText);
+            setTranslatedText(result)
+            console.log("result", result);
+        }
 
   return (
-    <div>TextInterface</div>
+    <div className='w-full h-screen'>
+        <form onSubmit={(e) => handleText(e)} className='w-full h-full flex items-center gap-4 p-10'>
+            {
+                translatedText && <p>{translatedText}</p>
+            }
+            <input className='text-black' type="text" onChange={(e) => setInputText(e.target.value)} />
+            <button>Submit</button>
+        </form>
+    </div>
   )
 }
 
